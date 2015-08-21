a073dcd3b9ec19adfb20cb71291c269373b36482
Vikas Raturi <rockingk10@gmail.com>
2015-08-22 00:59:02 +0530

    bug fixing

--- kNN.py
+++ kNN.py
@@ -18,7 +18,12 @@ class kNN():
         """Computes Euclidian Distance b/w two feature vectors
         X1 can be a numpy ndarray and x2 is numpy array
         """
-        return np.sqrt(np.sum((x1 - x2) ** 2, axis=1))
+        a= x1-x2
+        a2 = a**2
+        b = np.sum(a2, axis=1)
+        c = np.sqrt(b)
+        return c
+#         return np.sqrt(np.sum((x1 - x2) ** 2, axis=1))
 
     def fit(self, X, y):
         """takes input of features and corresponding labels
@@ -43,10 +48,10 @@ class kNN():
         # compute distance from all points
         for x1 in Xn:
             dist = self._euclidian_distance(self.X_data, x1)
-            dist = np.hstack((dist, self.y))
-            dist = dist[dist[:, 0].argsort(axis=0)[::-1]]
+            dist = np.vstack((dist, self.y)).T
+            dist = dist[dist[:, 0].argsort(axis=0)][:,-1]
             # get a vote from top k
-            pred = sts.mode(dist[0:self.k])[0][0][-1]
+            pred = sts.mode(dist[0:self.k])[0][0]
             preds.append(pred)
 
         return np.array(preds)
